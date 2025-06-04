8d1d2fe6e44ba3773e52351fe8a45b3335860323
Vagner Hirata <vagner.hirata@gmail.com>
2025-06-04 18:49:09 -0300

    Desafio batalha naval nivel novato

--- batalha_naval.c
+++ batalha_naval.c
@@ -2,6 +2,8 @@
 
 int main() {
 
+
+  // Tabuleiro da batalha naval
   int tabuleiro[10][10] = {
     {0,0,0,0,0,0,0,0,0,0},
     {0,0,0,0,0,0,0,0,0,0},
@@ -14,16 +16,20 @@ int main() {
     {0,0,0,0,0,0,0,0,0,0},
     {0,0,0,0,0,0,0,0,0,0}
   };
-
+  // Inicialização do Navio
   int navio[3] = {3,3,3};
   int navio2[3] = {3,3,3};
-
+ 
+  // Loop aninhado para imprimir o tabuleiro no console
   for(int i = 0; i < 10; i++) {
     for (int j = 0; j < 10; j++) {
+
+      // Condicional para criar uma nova linha a cada 10 casas
       if(j % 10 == 0){ 
         
         printf("\n");
       }
+      // imprime o tabuleiro no console
       printf("[%d]",tabuleiro[i][j]);
     }
   }
